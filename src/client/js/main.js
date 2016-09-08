0e330be1e005bebf3087bc5fc6ba03773c5c7874
Gina <gdebell@gmail.com>
2016-09-07 21:39:47 -0600

    user can click on delete button to remove their science project proposal.  The button is linked with this and is identified using the data-id attribute that matches up with this (proposal-id). next steps... validation uopon submission. update

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,9 +1,26 @@
 (function () {
 
-  //console.log('sanity check!');
+  console.log('sanity check!');
 
-  // $(document).on('click'(function() {
-  //   console.log('button has been clicked!');
-  // });
-
+  $(document).on('click', '.delete-btn', function() {
+    const message = confirm('Are you sure you want to delete your proposal?');
+    if (message) {
+      //console.log('button has been clicked!');
+      const $this = $(this);
+      //console.log($this);
+      const proposalID = $this.attr('data-id')
+      //console.log(proposalID);
+      $.ajax({
+        type: 'DELETE',
+        url: `/projects/delete/${proposalID}`
+      })
+      .done((data) => {
+        location.reload();
+        console.log(data);
+      })
+      .fail((err) => {
+        console.log(err);
+      });
+    }
+  });
 })();
